2b7a59639e7f1df1023240b0989d369ab254c034
Matias Arias <matt.arias182@gmail.com>
2022-11-27 20:07:49 -0300

    migrando footer a material ui

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,19 +1,46 @@
-import styles from "./Footer.module.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.footer}>
-        <h3 className={styles.footer__title}>Creado Por Matias Arias</h3>
-        <a
-          href="https://github.com/matiarias"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          sx={{
+            position: "sticky",
+            bottom: "0",
+            background: "linear-gradient(to right, #430059, #090075)",
+          }}
         >
-          <GitHubIcon fontSize="large" />
-        </a>
-      </div>
+          <Toolbar
+            sx={{ justifyContent: "center", alignItems: "center", gap: "12px" }}
+          >
+            <Typography variant="h6" component="div">
+              Creado por Matias Arias
+            </Typography>
+
+            <a
+              href="https://github.com/matiarias"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHubIcon fontSize="large" />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/matiasarias27/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedInIcon fontSize="large" />
+            </a>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </footer>
   );
 };
