aaa79b36907334103f3676a213e700e0383fd249
Matias Arias <matt.arias182@gmail.com>
2022-11-28 11:41:45 -0300

    migrando navbar a material ui

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -1,15 +1,33 @@
-import styles from "./NavBar.module.css";
+// import styles from "./NavBar.module.css";
+
+import { Box, AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+
 import MovieFilterIcon from "@mui/icons-material/MovieFilter";
 
 const NavBar = () => {
   return (
     <header>
-      <div className={styles.navbar}>
-        <div className={styles.navbarLeft}>
-          <MovieFilterIcon fontSize="large" />
-          <h1 className={styles.navbarTitle}>PelisSJ</h1>
-        </div>
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="static"
+          sx={{ background: "linear-gradient(to right, #007731, #003200)" }}
+        >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MovieFilterIcon fontSize="large" />
+            </IconButton>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              PelisSJ
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </header>
   );
 };
